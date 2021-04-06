85b941dc1676c86e885aa44ee18b294bd49e4b45
Luiz Amaral <alaluizamaral@gmail.com>
2021-04-06 20:02:32 -0300

    Realizado calculo de horas restantes, projetos ao total, em andamento e encerrados. Correcao para aparecer dados corretos no perfil

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -14,12 +14,15 @@ module.exports ={
         total: jobs.length
     }
 
+    let jobTotalHours = 0
+
     const updatedJobs = jobs.map((job) => {
 
         const remaining = JobUtils.remainingDays(job);
         const status = remaining <= 0 ? 'done' : 'progress';
 
         statusCount[status] += 1;
+        jobTotalHours = status === "progress" ? jobTotalHours + Number(job["daily-hours"]) : jobTotalHours
 
         return {
             ...job,
@@ -28,7 +31,10 @@ module.exports ={
             budget: JobUtils.calculateBudget(job, profile["value-hour"])
         };
     })
+    // qtd de horas que quero trabalhar dia MENOS quantidade de horas/dia de cada job em progress
+    
+    const freeHours = profile["hours-per-day"] - jobTotalHours;
 
-    return res.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount })
+    return res.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount, freeHours: freeHours});
     }
 }
